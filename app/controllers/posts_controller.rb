3139658b6e19e1ec99ef7533da3628c50f5cbfc0
Edward Iga Kigongo <igakigongo@gmail.com>
2019-09-04 18:41:35 +0200

    display posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      flash[:error] = "an error occured!"
+      render 'new'
+    end
   end
 
   def index
+    @posts = Post.includes(:user).all
   end
+
+  private 
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
