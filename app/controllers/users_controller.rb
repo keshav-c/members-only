28f33c00dfbb880fac9564db3ff81fc9fae288c9
keshav-c <keshav1985@gmail.com>
2019-09-05 22:27:41 +0530

    Add new users from web app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to root_url
+    else
+      flash.now[:error] = "Sign up failed!"
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users, only: [:new, :create]
   resources :posts, only: [:new, :create, :index]
 end
